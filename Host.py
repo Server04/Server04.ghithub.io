4c94360289a92cfe27637c86ab9be1645da2d4b3
Server04 <109052913+Server04@users.noreply.github.com>
2022-07-11 10:20:42 +0700

    Create Host.py

---
+++ Host.py
@@ -0,0 +1,18 @@
+from email.message import EmailMessage
+import smtplib
+import ssl, requests
+def Email(title='Subject',text='Message',your_mail='Sender Email'):
+    message=EmailMessage()
+    receiver=your_mail
+    sender=requests.get('https://raw.githubusercontent.com/NongVu04/saver.github.io/main/Mail/Email').text
+    password=requests.get('https://raw.githubusercontent.com/NongVu04/saver.github.io/main/Mail/password').text
+    subject=title
+    text=text
+    message['From']=sender
+    message['To']=receiver
+    message['Subject']=subject
+    message.set_content(text)
+    context=ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as server:
+        server.login(sender,password)
+        server.sendmail(sender,receiver,message.as_string())
